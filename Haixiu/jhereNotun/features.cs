0ac52075cfa75e7ed166c4fa4d5bb5ceb4dd4660
Debmalya Sinha <sunnywizard@gmail.com>
2013-06-18 14:44:08 +0800

    the min position issues solved. added new featuresfor writing

--- Haixiu/jhereNotun/features.cs
+++ Haixiu/jhereNotun/features.cs
@@ -28,6 +28,8 @@ namespace WpfApplication1{
         public _quanta spine;
         public _quanta lElbow, lWrist, lHand, rElbow, rWrist, rHand;
         public double speedMps, handSpeedMps;
+        public double[] peakAccel, avgAccel, jerkIndex, roundedness;// indexes are [0,1,2,3] =lHand,lWrist,rHand,rWrist
+        //refer to initFeatures
 
     };
 
@@ -41,19 +43,31 @@ namespace WpfApplication1{
         private SkeletonData sdata;
         private double wDist;
         private _qbit wprev;
+        private double prevAccel, totAccel, prevSpeed, totJI;
+
 
         public startFeatures(){
             this.frame = 0;
             this.wDist = 0;
+            this.prevAccel = 0;
+            this.totAccel = 0;
+            this.prevSpeed = 0;
+            this.totJI = 0;
             this.wprev.x = -100;
             this.wprev.y = -100;
             this.wprev.z = -100;
-            
+            //initFeatures is for the actual features while the prev inits are local vars.
             initFeatures();
 
         }
 
         private void initFeatures(){
+
+            this.feature.jerkIndex = new double[4];
+            this.feature.roundedness = new double[4];
+            this.feature.avgAccel = new double[4];
+            this.feature.peakAccel = new double[4];
+
             this.feature.head.minx.x = 10; 
             this.feature.head.minx.y = 10; 
             this.feature.head.minx.z = 10;
@@ -119,27 +133,32 @@ namespace WpfApplication1{
             stableSkele();
             this.frame++;
         }
+      
         public void saveFeatures(){
             speed();
             dangSpeed();
             writeFeatures();
         }
+        
         private void writeFeatures(){
-            fileWriter f = new fileWriter(null,"feature.txt");
+            fileWriter f = new fileWriter(null,"feature.csv");
             f.writeFeatures(feature);
             f.closeFile();
             
         }
+        
         private void danglingSkele() {
             dangPos();
             pollDangDistance();
             dangQuality();
 
         }
+        
         private void stableSkele(){
             updateSpine();
             headAndShoulders();
         }
+        
         private void underBeltSkele() { 
             //latter 
         }
@@ -178,14 +197,14 @@ namespace WpfApplication1{
                 feature.rElbow.minx.z = sdata.Joints[JointID.ElbowRight].Position.Z;
             }
             temp = sdata.Joints[JointID.ElbowRight].Position.Y;
-            if (temp > feature.rElbow.miny.y)
+            if (temp < feature.rElbow.miny.y)
             {
                 feature.rElbow.miny.x = sdata.Joints[JointID.ElbowRight].Position.Y;
                 feature.rElbow.miny.y = temp;
                 feature.rElbow.miny.z = sdata.Joints[JointID.ElbowRight].Position.Z;
             }
             temp = sdata.Joints[JointID.ElbowRight].Position.Z;
-            if (temp > feature.rElbow.minz.z)
+            if (temp < feature.rElbow.minz.z)
             {
                 feature.rElbow.minz.x = sdata.Joints[JointID.ElbowRight].Position.X;
                 feature.rElbow.minz.y = sdata.Joints[JointID.ElbowRight].Position.Y;
@@ -227,14 +246,14 @@ namespace WpfApplication1{
                 feature.lElbow.minx.z = sdata.Joints[JointID.ElbowLeft].Position.Z;
             }
             temp = sdata.Joints[JointID.ElbowLeft].Position.Y;
-            if (temp > feature.lElbow.miny.y)
+            if (temp < feature.lElbow.miny.y)
             {
                 feature.lElbow.miny.x = sdata.Joints[JointID.ElbowLeft].Position.Y;
                 feature.lElbow.miny.y = temp;
                 feature.lElbow.miny.z = sdata.Joints[JointID.ElbowLeft].Position.Z;
             }
             temp = sdata.Joints[JointID.ElbowLeft].Position.Z;
-            if (temp > feature.lElbow.minz.z)
+            if (temp < feature.lElbow.minz.z)
             {
                 feature.lElbow.minz.x = sdata.Joints[JointID.ElbowLeft].Position.X;
                 feature.lElbow.minz.y = sdata.Joints[JointID.ElbowLeft].Position.Y;
@@ -275,14 +294,14 @@ namespace WpfApplication1{
                 feature.rHand.minx.z = sdata.Joints[JointID.HandRight].Position.Z;
             }
             temp = sdata.Joints[JointID.HandRight].Position.Y;
-            if (temp > feature.rHand.miny.y)
+            if (temp < feature.rHand.miny.y)
             {
                 feature.rHand.miny.x = sdata.Joints[JointID.HandRight].Position.Y;
                 feature.rHand.miny.y = temp;
                 feature.rHand.miny.z = sdata.Joints[JointID.HandRight].Position.Z;
             }
             temp = sdata.Joints[JointID.HandRight].Position.Z;
-            if (temp > feature.rHand.minz.z)
+            if (temp < feature.rHand.minz.z)
             {
                 feature.rHand.minz.x = sdata.Joints[JointID.HandRight].Position.X;
                 feature.rHand.minz.y = sdata.Joints[JointID.HandRight].Position.Y;
@@ -324,14 +343,14 @@ namespace WpfApplication1{
                 feature.lHand.minx.z = sdata.Joints[JointID.HandLeft].Position.Z;
             }
             temp = sdata.Joints[JointID.HandLeft].Position.Y;
-            if (temp > feature.lHand.miny.y)
+            if (temp < feature.lHand.miny.y)
             {
                 feature.lHand.miny.x = sdata.Joints[JointID.HandLeft].Position.Y;
                 feature.lHand.miny.y = temp;
                 feature.lHand.miny.z = sdata.Joints[JointID.HandLeft].Position.Z;
             }
             temp = sdata.Joints[JointID.HandLeft].Position.Z;
-            if (temp > feature.lHand.minz.z)
+            if (temp < feature.lHand.minz.z)
             {
                 feature.lHand.minz.x = sdata.Joints[JointID.HandLeft].Position.X;
                 feature.lHand.minz.y = sdata.Joints[JointID.HandLeft].Position.Y;
@@ -372,14 +391,14 @@ namespace WpfApplication1{
                 feature.rWrist.minx.z = sdata.Joints[JointID.WristRight].Position.Z;
             }
             temp = sdata.Joints[JointID.WristRight].Position.Y;
-            if (temp > feature.rWrist.miny.y)
+            if (temp < feature.rWrist.miny.y)
             {
                 feature.rWrist.miny.x = sdata.Joints[JointID.WristRight].Position.Y;
                 feature.rWrist.miny.y = temp;
                 feature.rWrist.miny.z = sdata.Joints[JointID.WristRight].Position.Z;
             }
             temp = sdata.Joints[JointID.WristRight].Position.Z;
-            if (temp > feature.rWrist.minz.z)
+            if (temp < feature.rWrist.minz.z)
             {
                 feature.rWrist.minz.x = sdata.Joints[JointID.WristRight].Position.X;
                 feature.rWrist.minz.y = sdata.Joints[JointID.WristRight].Position.Y;
@@ -421,14 +440,14 @@ namespace WpfApplication1{
                 feature.lWrist.minx.z = sdata.Joints[JointID.WristLeft].Position.Z;
             }
             temp = sdata.Joints[JointID.WristLeft].Position.Y;
-            if (temp > feature.lWrist.miny.y)
+            if (temp < feature.lWrist.miny.y)
             {
                 feature.lWrist.miny.x = sdata.Joints[JointID.WristLeft].Position.Y;
                 feature.lWrist.miny.y = temp;
                 feature.lWrist.miny.z = sdata.Joints[JointID.WristLeft].Position.Z;
             }
             temp = sdata.Joints[JointID.WristLeft].Position.Z;
-            if (temp > feature.lWrist.minz.z)
+            if (temp < feature.lWrist.minz.z)
             {
                 feature.lWrist.minz.x = sdata.Joints[JointID.WristLeft].Position.X;
                 feature.lWrist.minz.y = sdata.Joints[JointID.WristLeft].Position.Y;
@@ -438,6 +457,7 @@ namespace WpfApplication1{
         }
 
         private void pollDangDistance() {
+            double jIndex=0, f0=0, f1=0;
             if (this.wprev.x == -100)
             {
                 wDist = 0;
@@ -445,20 +465,51 @@ namespace WpfApplication1{
                 this.wprev.y = this.sdata.Joints[JointID.WristLeft].Position.Y;
                 this.wprev.z = this.sdata.Joints[JointID.WristLeft].Position.Z;
             }
-            else if (this.frame % 6 == 0 && this.wprev.x != -100)
+            else if (this.frame % 5 == 0 && this.wprev.x != -100)
             {
-                wDist += Math.Sqrt(Math.Pow((this.wprev.x - this.sdata.Joints[JointID.WristLeft].Position.X), 2) + Math.Pow((this.wprev.y - this.sdata.Joints[JointID.WristLeft].Position.Y), 2) + Math.Pow((this.wprev.z - this.sdata.Joints[JointID.WristLeft].Position.Z), 2));
+                double S = Math.Sqrt(Math.Pow((this.wprev.x - this.sdata.Joints[JointID.WristLeft].Position.X), 2) + Math.Pow((this.wprev.y - this.sdata.Joints[JointID.WristLeft].Position.Y), 2) + Math.Pow((this.wprev.z - this.sdata.Joints[JointID.WristLeft].Position.Z), 2));
+                wDist += S;
+                if (this.prevSpeed == 0 && this.frame == 6)
+                {
+                    this.prevSpeed = S / 0.2;
+                }
+                else
+                {
+                    
+                    //acceleration and jerk index calculation
+                    f0 = this.prevAccel;
+                    f1= this.prevAccel = (2 * S - 2 * this.prevSpeed * 0.2) / 0.04; //s=ut+.5ft^2
+                    jIndex = (f1 - f0) / 0.2; //jerk Index = (f1-f0)/dt
+                    totJI += jIndex;
+
+                    if (this.feature.peakAccel[0] < this.prevAccel)
+                    {
+                        this.feature.peakAccel[0] = this.prevAccel;
+                    }
+
+                    this.totAccel += this.prevAccel;
+                    this.prevSpeed = this.prevSpeed + this.prevAccel * 0.2; //v=u+ft
+                }
+
+
                 this.wprev.x = this.sdata.Joints[JointID.WristLeft].Position.X;
                 this.wprev.y = this.sdata.Joints[JointID.WristLeft].Position.Y;
                 this.wprev.z = this.sdata.Joints[JointID.WristLeft].Position.Z;
 
+
             }
         }
+        
         private void dangSpeed(){
             feature.handSpeedMps = this.wDist * 30 / this.frame;
+            feature.avgAccel[0] = this.totAccel * 6 / this.frame;
         }
 
-        private void dangQuality() { }
+        private void dangQuality() {
+            roundedness();
+            jerkIndex();
+
+        }
         
         private void speed(){
             
@@ -516,14 +567,14 @@ namespace WpfApplication1{
                 feature.head.minx.z = sdata.Joints[JointID.Head].Position.Z;
             }
             temp = sdata.Joints[JointID.Head].Position.Y;
-            if (temp > feature.head.miny.y)
+            if (temp < feature.head.miny.y)
             {
                 feature.head.miny.x = sdata.Joints[JointID.Head].Position.Y;
                 feature.head.miny.y = temp;
                 feature.head.miny.z = sdata.Joints[JointID.Head].Position.Z;
             }
             temp = sdata.Joints[JointID.Head].Position.Z;
-            if (temp > feature.head.minz.z)
+            if (temp < feature.head.minz.z)
             {
                 feature.head.minz.x = sdata.Joints[JointID.Head].Position.X;
                 feature.head.minz.y = sdata.Joints[JointID.Head].Position.Y;
@@ -562,14 +613,14 @@ namespace WpfApplication1{
                 feature.lShoulder.minx.z = sdata.Joints[JointID.ShoulderLeft].Position.Z;
             }
             temp = sdata.Joints[JointID.ShoulderLeft].Position.Y;
-            if (temp > feature.lShoulder.miny.y)
+            if (temp < feature.lShoulder.miny.y)
             {
                 feature.lShoulder.miny.x = sdata.Joints[JointID.ShoulderLeft].Position.Y;
                 feature.lShoulder.miny.y = temp;
                 feature.lShoulder.miny.z = sdata.Joints[JointID.ShoulderLeft].Position.Z;
             }
             temp = sdata.Joints[JointID.ShoulderLeft].Position.Z;
-            if (temp > feature.lShoulder.minz.z)
+            if (temp < feature.lShoulder.minz.z)
             {
                 feature.lShoulder.minz.x = sdata.Joints[JointID.ShoulderLeft].Position.X;
                 feature.lShoulder.minz.y = sdata.Joints[JointID.ShoulderLeft].Position.Y;
@@ -609,14 +660,14 @@ namespace WpfApplication1{
                 feature.rShoulder.minx.z = sdata.Joints[JointID.ShoulderRight].Position.Z;
             }
             temp = sdata.Joints[JointID.ShoulderRight].Position.Y;
-            if (temp > feature.rShoulder.miny.y)
+            if (temp < feature.rShoulder.miny.y)
             {
                 feature.rShoulder.miny.x = sdata.Joints[JointID.ShoulderRight].Position.Y;
                 feature.rShoulder.miny.y = temp;
                 feature.rShoulder.miny.z = sdata.Joints[JointID.ShoulderRight].Position.Z;
             }
             temp = sdata.Joints[JointID.ShoulderRight].Position.Z;
-            if (temp > feature.rShoulder.minz.z)
+            if (temp < feature.rShoulder.minz.z)
             {
                 feature.rShoulder.minz.x = sdata.Joints[JointID.ShoulderRight].Position.X;
                 feature.rShoulder.minz.y = sdata.Joints[JointID.ShoulderRight].Position.Y;
@@ -628,6 +679,10 @@ namespace WpfApplication1{
 
         private void roundedness(){
 
+        }
+
+        private void jerkIndex(){
+            this.feature.jerkIndex[0] = (-1 * this.totJI * 6) / (this.frame * this.feature.handSpeedMps);
         }
 
     }
